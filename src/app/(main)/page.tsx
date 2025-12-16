c16f23ed382ab2fc0eeb03ad2f9f9b93942fda69
Magomedsoltan Kasumov <77916046+kenzWeb@users.noreply.github.com>
2025-12-16 13:49:35 +0300

    stories vugs

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -10,6 +10,7 @@ export default function Home() {
 			<main className={styles.main}>
 				<UserProfileSidebar />
 				<StoriesCarousel />
+				<div className='w-[30vw]'>TEST</div>
 			</main>
 		</>
 	)
--- src/features/UserStoryPreview/ui/UserStoryPreview.tsx
+++ src/features/UserStoryPreview/ui/UserStoryPreview.tsx
@@ -1,6 +1,12 @@
 import {Loader} from '@/entities/Loader'
 import {UserStoryPreviewItem} from '@/entities/User'
 import {useGetStories} from '@/shared/api/hooks'
+
+import {
+	Carousel,
+	CarouselContent,
+	CarouselItem,
+} from '@/shared/components/ui/Carousel'
 import styles from './UserStoryPreview.module.css'
 
 export const UserStoryPreview = () => {
@@ -15,16 +21,26 @@ export const UserStoryPreview = () => {
 					isLoading={isLoading}
 				/>
 			)}
-
-			{users.map((user) =>
-				(user.stories?.length ?? 0) > 0 ? (
-					<UserStoryPreviewItem
-						key={user.id}
-						userInfo={user}
-						stories={user.stories ?? []}
-					/>
-				) : null,
-			)}
+			<Carousel
+				className={styles.carousel}
+				opts={{
+					align: 'start',
+					dragFree: true,
+				}}
+			>
+				<CarouselContent className={styles.carouselContent}>
+					{users.map((user) =>
+						(user.stories?.length ?? 0) > 0 ? (
+							<CarouselItem className={styles.carouselItem} key={user.id}>
+								<UserStoryPreviewItem
+									userInfo={user}
+									stories={user.stories ?? []}
+								/>
+							</CarouselItem>
+						) : null,
+					)}
+				</CarouselContent>
+			</Carousel>
 		</div>
 	)
 }
