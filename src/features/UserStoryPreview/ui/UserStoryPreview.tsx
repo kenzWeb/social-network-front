a810ae69773837e5ec1211a0181322c9ff9cf579
Magomedsoltan Kasumov <77916046+kenzWeb@users.noreply.github.com>
2025-12-23 21:00:36 +0300

    replace slider library

--- src/features/UserStoryPreview/ui/UserStoryPreview.tsx
+++ src/features/UserStoryPreview/ui/UserStoryPreview.tsx
@@ -1,47 +1,55 @@
-import {Loader} from '@/entities/Loader'
-import {UserStoryPreviewItem} from '@/entities/User'
-import {useGetStories} from '@/shared/api/hooks'
+'use client'
 
-import {
-	Carousel,
-	CarouselContent,
-	CarouselItem,
-} from '@/shared/components/ui/Carousel'
+import { Loader } from '@/entities/Loader'
+import { UserStoryPreviewItem } from '@/entities/User'
+import { useGetStories } from '@/shared/api/hooks'
+import { FreeMode, Navigation } from 'swiper/modules'
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+import 'swiper/css'
+import 'swiper/css/free-mode'
+import 'swiper/css/navigation'
+
 import styles from './UserStoryPreview.module.css'
 
 export const UserStoryPreview = () => {
-	const {users, isLoading, isPending} = useGetStories()
+	const { users, isLoading, isPending } = useGetStories()
 
 	return (
 		<div className={styles.wrapper}>
 			{isLoading && (
 				<Loader
-					className='size-10'
+					className="size-10"
 					isPending={isPending}
 					isLoading={isLoading}
 				/>
 			)}
-			<Carousel
-				className={styles.carousel}
-				opts={{
-					align: 'start',
-					dragFree: true,
-					containScroll: false,
+			<Swiper
+				className={styles.swiper}
+				modules={[Navigation, FreeMode]}
+				slidesPerView="auto"
+				spaceBetween={45}
+				freeMode={{
+					enabled: true,
+					sticky: false,
+					momentumBounce: false
 				}}
+				navigation
 			>
-				<CarouselContent className={styles.carouselContent}>
-					{users.map((user) =>
-						(user.stories?.length ?? 0) > 0 ? (
-							<CarouselItem className={styles.carouselItem} key={user.id}>
-								<UserStoryPreviewItem
-									userInfo={user}
-									stories={user.stories ?? []}
-								/>
-							</CarouselItem>
-						) : null,
-					)}
-				</CarouselContent>
-			</Carousel>
+				{users.map(user =>
+					(user.stories?.length ?? 0) > 0 ? (
+						<SwiperSlide
+							className={styles.slide}
+							key={user.id}
+						>
+							<UserStoryPreviewItem
+								userInfo={user}
+								stories={user.stories ?? []}
+							/>
+						</SwiperSlide>
+					) : null
+				)}
+			</Swiper>
 		</div>
 	)
 }
