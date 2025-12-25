e3174c54f1737f4e90fc66d0933ff516e4835b1a
Magomedsoltan Kasumov <77916046+kenzWeb@users.noreply.github.com>
2025-12-25 20:41:47 +0300

    add RecentActivitySidebar components

---
+++ src/widgets/RecentActivitySidebar/ui/RecentActivitySidebar.tsx
@@ -0,0 +1,9 @@
+import styles from './RecentActivitySidebar.module.css'
+
+export const RecentActivitySidebar = () => {
+	return (
+		<div className={styles.wrapper}>
+			RecentActivitySidebar
+		</div>
+	)
+}
---
+++ src/widgets/RecentActivitySidebar/ui/index.ts
@@ -0,0 +1,1 @@
+export { RecentActivitySidebar } from './RecentActivitySidebar'
