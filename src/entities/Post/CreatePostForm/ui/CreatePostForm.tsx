8b3f392ab1cb23ebb8f56c7ed8fe3fda224c9e4e
Magomedsoltan Kasumov <77916046+kenzWeb@users.noreply.github.com>
2026-03-23 14:35:52 +0300

    Post entity

---
+++ src/entities/Post/CreatePostForm/ui/CreatePostForm.tsx
@@ -0,0 +1,10 @@
+import styles from './CreatePostForm.module.css'
+import { CreatePostFormProps } from '../types'
+
+export const CreatePostForm = (props: CreatePostFormProps) => {
+	return (
+		<div className={styles.wrapper}>
+			CreatePostForm
+		</div>
+	)
+}
---
+++ src/entities/Post/PostCard/ui/PostCard.tsx
@@ -0,0 +1,10 @@
+import styles from './PostCard.module.css'
+import { PostCardProps } from '../types'
+
+export const PostCard = (props: PostCardProps) => {
+	return (
+		<div className={styles.wrapper}>
+			PostCard
+		</div>
+	)
+}
