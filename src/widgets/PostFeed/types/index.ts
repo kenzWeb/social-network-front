65bdb25170efd573077989b8bb117a8c4970d642
Magomedsoltan Kasumov <77916046+kenzWeb@users.noreply.github.com>
2026-03-23 14:31:59 +0300

    Post component create

---
+++ src/widgets/PostFeed/types/index.ts
@@ -0,0 +1,1 @@
+export interface PostFeedProps { // Добавьте типы здесь }
---
+++ src/widgets/PostFeed/ui/PostFeed.tsx
@@ -0,0 +1,10 @@
+import styles from './PostFeed.module.css'
+import { PostFeedProps } from '../types'
+
+export const PostFeed = (props: PostFeedProps) => {
+	return (
+		<div className={styles.wrapper}>
+			PostFeed
+		</div>
+	)
+}
